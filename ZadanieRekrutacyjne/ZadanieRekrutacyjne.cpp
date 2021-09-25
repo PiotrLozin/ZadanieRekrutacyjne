da14589a7d88652396dfff5dd38e9dc3be061434
PiotrLozin <piotrlozin1998@gmail.com>
2021-09-25 14:45:25 +0200

    Removed unnecessary elements

--- ZadanieRekrutacyjne/ZadanieRekrutacyjne.cpp
+++ ZadanieRekrutacyjne/ZadanieRekrutacyjne.cpp
@@ -21,7 +21,7 @@ int main()
 {
     ifstream ifs("sample_data.bin", ios::binary);
     ofstream ofs("data.csv");
-    char comunicate[1024];
+    char comunicate[1025];
     int k = 0;
 
     if (ifs.good())
@@ -30,29 +30,17 @@ int main()
         {
             char* temp = new char[sizeof(Record)];
             ifs.read(temp, sizeof(Record));
-
             Record* first_record = (Record*)temp;
-
-            cout << first_record->numer_16_bit << " ";
-            ofs << first_record->numer_16_bit << ";";
-
-            //cout << bitset<8>(first_record->part_of_comunicate) << " ";
             comunicate[k] = first_record->part_of_comunicate;
             k++;
 
-            //cout << first_record->padding << " ";
-
-            cout << first_record->big_endian_32_bit << " ";
+            ofs << first_record->numer_16_bit << ";";
             ofs << first_record->big_endian_32_bit << ";";
-
-            cout << first_record->number_floating << " ";
             ofs << first_record->number_floating << ";";
 
-            //cout << first_record->bit_field << " ";
             unsigned short number_7_bit = 0;
             for (int i = 7; i >= 1; i--) // or (int i = 0; i < 8; i++)  if you want reverse bit order in bytes
                 number_7_bit += (unsigned short)((first_record->bit_field[2] >> i) & 1);
-            cout << number_7_bit << endl;
             ofs << number_7_bit << "\n";
             delete first_record;
         }
